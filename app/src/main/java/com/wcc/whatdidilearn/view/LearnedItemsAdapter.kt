d6b707e0ade9875d4c5839bc900574e97516fd62
JulianeOlivCosta <73131651+JulianeOlivCosta@users.noreply.github.com>
2020-10-31 14:39:47 -0300

    creating view holder

--- app/src/main/java/com/wcc/whatdidilearn/view/LearnedItemsAdapter.kt
+++ app/src/main/java/com/wcc/whatdidilearn/view/LearnedItemsAdapter.kt
@@ -2,8 +2,12 @@ package com.wcc.whatdidilearn.view
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+import com.wcc.whatdidilearn.R
 import com.wcc.whatdidilearn.entities.ItemLearned
+import org.w3c.dom.Text
 
 class LearnedItemsAdapter: RecyclerView.Adapter<LearnedItemsAdapter.LearnedItemViewHolder>() {
 
@@ -11,9 +15,18 @@ class LearnedItemsAdapter: RecyclerView.Adapter<LearnedItemsAdapter.LearnedItemV
 
 
     inner class LearnedItemViewHolder (itemView: View): RecyclerView.ViewHolder (itemView){
+        val titleItem: TextView = itemView.findViewById(R.id.itemTitle)
+        val descriptionItem: TextView = itemView.findViewById(R.id.itemDescription)
+        val levelItem: View = itemView.findViewById(R.id.itemLevel)
 
+        fun bind(title: String, description:String, color: Int){
+            titleItem.text = title,
+            descriptionItem.text = description,
+            levelItem.setBackgroundResource(color)
+        }
     }
 
+    //tela - dados
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LearnedItemViewHolder {
         TODO("Not yet implemented")
     }
