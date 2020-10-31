1496ece6a7d53ea3c5520975d00a24007ea42fa7
JulianeOlivCosta <73131651+JulianeOlivCosta@users.noreply.github.com>
2020-10-31 15:25:23 -0300

    chamando recycle view na main activity

--- app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
@@ -2,11 +2,20 @@ package com.wcc.whatdidilearn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.wcc.whatdidilearn.R
+import com.wcc.whatdidilearn.data.DatabaseItems
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recycleView = findViewById<RecyclerView>(R.id.items_recycle_views)
+        val adapter = LearnedItemsAdapter()
+        recycleView.adapter = adapter
+
+        val learnedItemsList = DatabaseItems().getAll()
+        adapter.data = learnedItemsList
     }
 }
