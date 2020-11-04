007d3bb34824723068b41fd9468fe7ede6ece89e
JulianeOlivCosta <73131651+JulianeOlivCosta@users.noreply.github.com>
2020-11-03 21:17:04 -0300

    adding Entity tabelas

--- app/src/main/java/com/wcc/whatdidilearn/entities/ItemLearned.kt
+++ app/src/main/java/com/wcc/whatdidilearn/entities/ItemLearned.kt
@@ -1,7 +1,21 @@
 package com.wcc.whatdidilearn.entities
 
-data class ItemLearned(
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "learned_item")
+data class ItemLearned (
+    @ColumnInfo(name = "item_title")
     val title: String,
+
+    @ColumnInfo(name = "item_description")
     val description: String,
-    val understandingLevel: UnderstandingLevel
+
+    @ColumnInfo(name = "item_understanding_level")
+    val understanding_level: UnderstandingLevel, //nao é um tipo primario, fazer um conversao, pois é diferente do tipo primario.
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "item_id")
+    val id: Int = 0
 )
