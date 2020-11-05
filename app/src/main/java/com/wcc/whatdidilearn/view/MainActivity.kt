88bbb6c14041c78f21e52f534d0b093c2e288d7a
JulianeOlivCosta <73131651+JulianeOlivCosta@users.noreply.github.com>
2020-11-04 22:23:04 -0300

    "creating dataBase, editing MainActivity.kt to access. Creating another activity to add NewLearnedItem.kt "

--- app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/com/wcc/whatdidilearn/view/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.wcc.whatdidilearn.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import com.wcc.whatdidilearn.NewLearnedItem
 import com.wcc.whatdidilearn.R
 import com.wcc.whatdidilearn.data.DatabaseItems
 import com.wcc.whatdidilearn.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +22,17 @@ class MainActivity : AppCompatActivity() {
         val adapter = LearnedItemsAdapter()
         recycleView.adapter = adapter
 
-        val learnedItemsList = DatabaseItems.getAll()
-        adapter.data = learnedItemsList
+        val database = DatabaseItems.getDatabase(this, CoroutineScope(Dispatchers.IO))
+        val dao = database.learnedItemDao()
+        val itemsList = dao.getAll()
+        itemsList.observe(this, androidx.lifecycle.Observer { items ->
+            adapter.data = items
+        })
+
+        val addfab = binding.addFab
+        addfab.setOnClickListener{
+            var intent = Intent(this, NewLearnedItem::class.java)
+            startActivity(intent)
+        }
     }
 }
